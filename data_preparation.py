87e75bcf52b465eb28ad7ee7760058e33ed84888
Kaja Brąz <kajabraz@gmail.com>
2022-01-23 20:21:26 +0100

    text normalisation changed

--- data_preparation.py
+++ data_preparation.py
@@ -4,11 +4,21 @@ START_TAG = '<START> '
 END_TAG = ' <END>'
 
 
-def adjust_text(text: str) -> str:
-    text = ''.join([ch for ch in text if ch.isalnum() or ch in ['.', ',', ' ']])
-    text_separated_commas = text.replace(', ', ' , ')
-    return text_separated_commas
+# TODO handle not replacing the apostrophes in words (ex. don't)
+# TODO handle e-mail addresses and special symbols appearing between letters, i.e. in the following context: \w_\w
 
 
-def tokenize_sentences(text: str) -> [str]:
-    return [START_TAG + sent + END_TAG for sent in re.split(r'\.\s', text)]
+def normalize_text(text: str, full=True) -> str:
+    lower = text.lower()
+    if full:
+        normalized = re.sub(r'^\w\s', '', lower)
+    else:
+        normalized = re.sub(r'[^-%\.\w\s]', '', lower)
+    return normalized
+
+
+def adjust_and_tokenize(text: str) -> [str]:
+    text = normalize_text(text, False)
+    sentences = [START_TAG + sent + END_TAG for sent in re.split(r'\.\s', text)]
+    sentences = [sent.replace('.', '') for sent in sentences]
+    return sentences
