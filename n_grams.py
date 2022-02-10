5c50b335465fec86c1a1112b195b8bbdd6dd6bfa
Kaja Brąz <kajabraz@gmail.com>
2022-02-10 23:39:36 +0100

    adjustment to the chosen type of the data (nltk's brown corpus)

--- n_grams.py
+++ n_grams.py
@@ -1,13 +1,13 @@
 from collections import defaultdict
 
-from src import data_preparation
+from NextWordPredictor.src import data_preparation
 
 
 def create_bigrams(corpus: [str]) -> {(str, str): int}:
-    normalized_tokenized = data_preparation.adjust_and_tokenize(corpus)
     bigram_counts = defaultdict(int)
+    normalized_tokenized = data_preparation.adjust_and_normalize_nltk_brawn(corpus)
     for sent in normalized_tokenized:
-        words = sent.split(' ')
+        words = sent
         for i in range(len(words) - 2):
             bigram = (words[i], words[i + 1])
             bigram_counts[bigram] += 1
