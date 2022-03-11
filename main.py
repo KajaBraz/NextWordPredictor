df2ca5d2dd828181f27d11a2b419a39e47bb924b
KajaBraz <kajabraz@gmail.com>
2022-03-11 02:18:15 +0100

    moving the part with program's execution to main; enabling prediction function reusabiliy

--- main.py
+++ main.py
@@ -6,18 +6,23 @@ lexicon = database.lexicon
 n_grams = Counter(database.normalized_bigrams)
 
 if __name__ == '__main__':
-    next_word.predict_next_word(n_grams, lexicon, constants.MAX_N_GRAMS_SIZE)
+    whole_text = ''
+    current_n_gram = [constants.START_TAG] * (constants.MAX_N_GRAMS_SIZE - 1)
+    current_word = current_n_gram[-1]
 
-    # print(len(lexicon))
+    while current_word != '.':
+        possible_next_words = next_word.predict_next_word(current_n_gram, n_grams, lexicon, constants.MAX_N_GRAMS_SIZE)
 
-    # print(type(lexicon))
-    # print(lexicon.most_common(10))
-    # print(lexicon.most_common()[-10:])
-    # print(sorted(lexicon.values()))
+        print(f'SUGGESTED WORDS:\n{possible_next_words}')
+        input_text = input('PASTE THE NUMBER THAT CORRESPONDS TO THE WORD (FROM 1 TO 10),'
+                           ' TO CHOOSE ONE OF THE WORDS OR PASTE YOUR OWN WORD AND PRESS ENTER\n')
+        input_words = input_text.split(' ')
+        current_word = input_words[-1]
 
-    # print(len(n_grams))
-    # print(type(n_grams))
-    # print(n_grams.most_common(10))
-    # print(n_grams.most_common()[-10:])
+        if current_word.isnumeric() and 1 <= int(current_word) <= constants.SUGGESTED_WORDS_NUMBER:
+            current_word = possible_next_words[int(current_word) - 1]
 
-    # print(len([(word, count) for word, count in lexicon.items() if count>10]))
+        current_n_gram = current_n_gram[1:] + [current_word] if len(
+            current_n_gram) >= constants.MAX_N_GRAMS_SIZE else current_n_gram + [current_word]
+        whole_text = whole_text + ' ' + current_word
+        print(f'YOUR TEXT:\n{whole_text}\n')
--- src/next_word.py
+++ src/next_word.py
@@ -15,38 +15,22 @@ def get_next_words(last_words: Tuple[str], all_n_grams_dict: {int: Counter}, max
     return possible_next_n_grams
 
 
-def predict_next_word(n_grams_dict: Counter, lexicon: Counter, max_n_grams_size: int) -> None:
-    print('PREDICT', n_grams_dict.keys())
-    lexicon.pop(constants.START_TAG)
-    lexicon.pop(constants.END_TAG)
-    current_word = constants.START_TAG
-    whole_text = ''
-    current_n_gram = [constants.START_TAG] * (max_n_grams_size - 1)
-    while current_word != '.':
-        # print('***')
-        # print(current_n_gram)
-        # print('***').
-        possible_next_words = get_next_words(tuple(current_n_gram), n_grams_dict, max_n_grams_size)
-        missing = constants.SUGGESTED_WORDS_NUMBER - len(possible_next_words)
+def predict_next_word(current_n_gram: str, n_grams_dict: Counter, lexicon: Counter, max_n_grams_size: int) -> [str]:
+    if constants.START_TAG in lexicon:
+        lexicon.pop(constants.START_TAG)
+    if constants.END_TAG in lexicon:
+        lexicon.pop(constants.END_TAG)
 
-        # TODO when suggesting most common words from lexicon (in case of missing n_grams in dictionary), prevent
-        #  duplications (it can happen that we have just one matching n_gram in the n_gram dict and if the suggested
-        #  word is ex. "the", the rest of the suggested words will be taken from the lexicon and "the" can be the most
-        #  common word; this way it will be doubled in suggestions)
+    possible_next_words = get_next_words(tuple(current_n_gram), n_grams_dict, max_n_grams_size)
+    missing = constants.SUGGESTED_WORDS_NUMBER - len(possible_next_words)
 
-        if missing > 0:
-            default_words = [word for word, count in lexicon.most_common(missing)]
-            possible_next_words = possible_next_words + default_words
-        print(f'SUGGESTED WORDS:\n{possible_next_words}')
-        input_text = input('PASTE THE NUMBER THAT CORRESPONDS TO THE WORD (FROM 1 TO 10),'
-                           ' TO CHOOSE ONE OF THE WORDS OR PASTE YOUR OWN WORD AND PRESS ENTER\n')
-        input_words = input_text.split(' ')
-        current_word = input_words[-1]
+    # TODO when suggesting most common words from lexicon (in case of missing n_grams in dictionary), prevent
+    #  duplications (it can happen that we have just one matching n_gram in the n_gram dict and if the suggested
+    #  word is ex. "the", the rest of the suggested words will be taken from the lexicon and "the" can be the most
+    #  common word; this way it will be doubled in suggestions)
 
-        if current_word.isnumeric() and 1 <= int(current_word) <= constants.SUGGESTED_WORDS_NUMBER:
-            current_word = possible_next_words[int(current_word) - 1]
+    if missing > 0:
+        default_words = [word for word, count in lexicon.most_common(missing)]
+        possible_next_words = possible_next_words + default_words
 
-        current_n_gram = current_n_gram[1:] + [current_word] if len(
-            current_n_gram) >= max_n_grams_size else current_n_gram + [current_word]
-        whole_text = whole_text + ' ' + current_word
-        print(f'YOUR TEXT:\n{whole_text}\n')
+    return possible_next_words
