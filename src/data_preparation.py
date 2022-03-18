6be0e0d21d2b7deb1c9c0336cc0c09549ff6bd85
KajaBraz <kajabraz@gmail.com>
2022-03-18 01:20:02 +0100

    renaming

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -3,8 +3,8 @@ import re
 from src import constants
 
 
-def adjust_and_normalize_nltk_brawn(corpus: [[str]], n_grams: int) -> [[str]]:
-    if n_grams < 1:
+def adjust_and_normalize_nltk_brawn(corpus: [[str]], n_grams_size: int) -> [[str]]:
+    if n_grams_size < 1:
         return
     processed_corpus = []
     pattern_special_chars_beginning = re.compile(r'^\W+$')  # match special chars at the beginning of strings ("''")
@@ -14,6 +14,6 @@ def adjust_and_normalize_nltk_brawn(corpus: [[str]], n_grams: int) -> [[str]]:
         filtered = map(lambda w: pattern_special_chars_beginning.sub('', w), lower)
         filtered = map(lambda w: pattern_special_chars_end.sub('', w), filtered)
         filtered = filter(lambda w: len(w) != 0, filtered)
-        updated_sent = [constants.START_TAG] * (n_grams) + list(filtered) + [constants.END_TAG]
+        updated_sent = [constants.START_TAG] * n_grams_size + list(filtered) + [constants.END_TAG]
         processed_corpus.append(updated_sent)
     return processed_corpus
